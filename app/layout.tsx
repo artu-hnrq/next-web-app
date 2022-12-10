a52eba4c358bee144b3ae78e17503dcb1cda09bb
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2022-12-10 04:17:00 -0300

    feat: configure sass and tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
-import './globals.css'
+import './globals.scss'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <head/>
       <body>{children}</body>
     </html>
   )
