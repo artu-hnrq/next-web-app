822c53b32f46fbf44f0a9cffdb29743731416ab0
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2022-12-21 00:02:52 -0300

    feat: organize client and server components inside their own subdirectory

--- components/index.js
+++
@@ -1,5 +0,0 @@
-import Bar from './Bar';
-import Tours from './Tours';
-import Logo from './Logo';
-
-export { Bar, Logo, Tours };
---
+++ components/server/index.js
@@ -0,0 +1,11 @@
+import Bar from './Bar';
+import Banner from './Banner';
+import Tours from './Tours';
+import Logo from './Logo';
+
+export {
+    Bar,
+    Banner,
+    Logo,
+    Tours,
+};
